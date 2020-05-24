4845e3c5d28a909ea7959ffe81e4d54b848a6bc9
David-Lor <17401854+david-lor@users.noreply.github.com>
2020-05-24 17:30:37 +0200

    (WIP5) test

--- tests/entity.delete.test.js
+++ tests/entity.delete.test.js
@@ -3,22 +3,30 @@ const faker = require("faker");
 const { expect } = require("chai");
 const request = require("@arangodb/request");
 const { getEntityExisting, stringify } = require("./utils.js");
+const { getEntity } = require("../src/database.js");
 const { baseUrl } = module.context;
 
 describe('entity get', () => {
-    it('should get the entity', () => {
+    it('should delete the entity', () => {
         const entity = getEntityExisting();
         const url = `${baseUrl}/entities/${entity.id}`;
 
-        const response = request.get(url);
-        expect(response.statusCode).to.be.equal(200, stringify({ response, url, existingEntity: entity }));
+        const response = request.delete(url);
+        expect(response.statusCode).to.be.equal(204, stringify({ response, url, existingEntity: entity }));
 
-        expect(response.json).to.be.deep.equal(entity, stringify({ result: response.json, expected: entity }));
+        let error = null;
+        try {
+            getEntity(entity.id)
+        } catch (err) {
+            error = err;
+        }
+
+        expect(error).to.be.not.null;
     });
 
     it('should fail to get a non existing entity', () => {
         const url = `${baseUrl}/entities/${faker.random.uuid()}`;
-        const response = request.get(url);
+        const response = request.delete(url);
         expect(response.statusCode).to.be.equal(404, stringify({ response, url }));
     });
 });
